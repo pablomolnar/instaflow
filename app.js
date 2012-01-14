65d9071d62300b06961bfd330342a0dfc49f8cac
Pablo Molnar <pablomolnar@gmail.com>
2012-01-14 13:39:12 -0300

    Add socket.io

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-/*
- * Module dependencies.
- */
-
-//require.paths.unshift('./node_modules')
-
+// Module dependencies.
 var express = require('./node_modules/express')
   , routes = require('./routes')
+  , io = require('socket.io');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer()
+  , io = io.listen(app);
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -29,8 +25,26 @@ app.configure('production', function(){
 });
 
 // Routes
+app.get('/', routes.index);  
 
-app.get('/', routes.index);
+// Callback URL //
+app.get('/callback', function(req, res){
+	res.send(req.query['hub.challenge']);
+});
+
+app.post('/callback', function(req, res){  
+	var pics = req.body;  
+	io.sockets.emit('pics', pics);     
+	res.send('ok');
+}); 
+       
+// Web sockets
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { msg: 'connected' });
+  socket.on('my other event', function (data) {
+    console.log(data); 
+  });
+});
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
--- test/suite.js
+++ test/suite.js
@@ -6,7 +6,6 @@ var assert = require('assert'),
 	
                                                     
 describe('Basic Suite', function(){
-	
   it('should return Welcome to Express', function(done) {    	
     http.get({ path: '/', port: 3000 }, function(response) {                   
 	  // Check status
@@ -16,10 +15,62 @@ describe('Basic Suite', function(){
 	  response.on('data', function (chunk) {    
 	    chunk.toString().should.include('Welcome to Express')
 	  });    
-	
+
       done();
     })
   })
+})
 
+describe('Callback URL', function(){
+  it('should respond to the GET request with the hub.challenge ', function(done) {    	
+    http.get({ path: '/callback?hub.mode=subscribe&hub.challenge=15f7d1a91c1f40f8a748fd134752feb3&hub.verify_token=myVerifyToken', port: 3000 }, function(response) {                   
+	  // Check status
+      response.should.have.status(200);        
 
+      // Check body
+	  response.on('data', function (chunk) {    
+	    chunk.toString().should.equal('15f7d1a91c1f40f8a748fd134752feb3')
+	  });    
+
+      done();
+    })
+  }); 
+
+  it('should recieve POST eveents of new photos', function(done) { 
+	 
+	// Move to fixtures
+	var body =
+	[
+	    {
+	        "subscription_id": "1",
+	        "object": "user",
+	        "object_id": "1234",
+	        "changed_aspect": "media",
+	        "time": 1297286541
+	    },
+	    {
+	        "subscription_id": "2",
+	        "object": "tag",
+	        "object_id": "nofilter",
+	        "changed_aspect": "media",
+	        "time": 1297286541
+	    }
+	];	
+
+	var req = http.request({ port: 3000, path: '/callback', method: 'POST' }, function(res) {
+      // Check status
+	  res.should.have.status(200);        
+	
+	  // Check body
+	  res.on('data', function (chunk) {
+		
+	  });
+	
+	  done();    
+	});                         
+	
+	// write data to request body
+	req.write(JSON.stringify(body));
+	req.end();
+  })
 })
